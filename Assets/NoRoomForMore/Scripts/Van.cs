b8991286e822838b4ef727cbbd856442237e92f7
springruse <152939330+springruse@users.noreply.github.com>
2026-03-01 18:33:34 -0700

    added a bunch of assets and rescripted van

--- Assets/NoRoomForMore/Scripts/Van.cs
+++ Assets/NoRoomForMore/Scripts/Van.cs
@@ -1,6 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Van : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class Van : MonoBehaviour
     [SerializeField] Transform spawnPoint;
     [SerializeField] float launchForce = 5f;
     [SerializeField] float upwardForce = 1f;
+    [SerializeField] int minFurniture = 15;
+    [SerializeField] int maxFurniture = 15;
+    [SerializeField] GameObject[] allFurniturePrefabs;
 
     [SerializeField] float interactRange = 3f;
     private Transform player;
@@ -31,11 +35,22 @@ public class Van : MonoBehaviour
 
     void Interact()
     {
+        // empty van
+        if (Furn.Length == 0)
+        {
+            RestockVan();
+            return;
+        }
+
         VanDoor.SetActive(true);
         //Audio.Play();
 
         int randomIndex = Random.Range(0, Furn.Length);
         GameObject furniture = Instantiate(Furn[randomIndex], spawnPoint.position, Random.rotation);
+
+        List<GameObject> furnList = new List<GameObject>(Furn);
+        furnList.RemoveAt(randomIndex);
+        Furn = furnList.ToArray();
 
         Rigidbody rb = furniture.GetComponent<Rigidbody>();
         if (rb == null)
@@ -43,6 +58,25 @@ public class Van : MonoBehaviour
 
         Vector3 shootDirection = spawnPoint.forward * launchForce + Vector3.up * upwardForce;
         rb.AddForce(shootDirection, ForceMode.Impulse);
+
+
+    }
+
+    public void RestockVan()
+    {
+        List<GameObject> newStock = new List<GameObject>();
+
+        int amountToAdd = Random.Range(minFurniture, maxFurniture + 1);
+
+        for (int i = 0; i < amountToAdd; i++)
+        {
+            int randomIndex = Random.Range(0, allFurniturePrefabs.Length);
+            newStock.Add(allFurniturePrefabs[randomIndex]);
+        }
+
+        Furn = newStock.ToArray();
+
+        Debug.Log($"Van restocked with {Furn.Length} furniture pieces!");
     }
 
     void OnDrawGizmosSelected()
